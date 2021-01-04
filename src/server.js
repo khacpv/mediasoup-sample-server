1b5a0d5a511acbd929916b09f69255f7fe87d7bc
khacpv <khacpv@gmail.com>
2021-01-04 23:23:38 +0700

    update code

--- src/server.js
+++ src/server.js
@@ -1,24 +1,24 @@
 const fs = require('fs');
-// const https = require('https');
-const express = require('express');
+const https = require('https');
+// const express = require('express');
 const WebSocket = require('ws');
 
 const { handleSocket } = require('./socket');
 const { initializeWorkers } = require('./worker');
 
-// const HTTPS_OPTIONS = Object.freeze({
-//   cert: fs.readFileSync('./ssl/fullchain.pem'),
-//   key: fs.readFileSync('./ssl/privkey.pem')
-// });
+const HTTPS_OPTIONS = Object.freeze({
+  cert: fs.readFileSync('./ssl/fullchain.pem', 'utf8'),
+  key: fs.readFileSync('./ssl/privkey.pem', 'utf8')
+});
 
-// const httpsServer = https.createServer(HTTPS_OPTIONS);
-const app = express();
+const httpsServer = https.createServer(HTTPS_OPTIONS);
+// const app = express();
 
 const wss = new WebSocket.Server(
   {
     maxPayload: 200000000,
-    noServer: true,
-    // server: httpsServer
+    // noServer: true,
+    server: httpsServer
   },
   () => console.log('WebSocket.Server started at port 8080')
 );
@@ -33,20 +33,20 @@ function heartbeat() {
   try {
     await initializeWorkers();
 
-    // httpsServer.listen(8883, () =>
-    //   console.log('websocket SSL server running on port 443')
-    // );
+    httpsServer.listen(8883, () =>
+      console.log('websocket SSL server running on port 443')
+    );
 
-    const server = app.listen(3000, () => {
-      console.log('websocket SSL server running on port 3000');
-    });
-    console.log('__dirname', __dirname);
-    app.use(express.static(__dirname + '/../public/dist'));
-    server.on('upgrade', (request, socket, head) => {
-      wsServer.handleUpgrade(request, socket, head, (socket) => {
-        wsServer.emit('connection', socket, request);
-      });
-    });
+    // const server = app.listen(3000, () => {
+    //   console.log('websocket SSL server running on port 3000');
+    // });
+    // console.log('__dirname', __dirname);
+    // app.use(express.static(__dirname + '/../public/dist'));
+    // server.on('upgrade', (request, socket, head) => {
+    //   wsServer.handleUpgrade(request, socket, head, (socket) => {
+    //     wsServer.emit('connection', socket, request);
+    //   });
+    // });
   } catch (error) {
     console.error('Failed to initialize workers [error:%o]', error);
   }
